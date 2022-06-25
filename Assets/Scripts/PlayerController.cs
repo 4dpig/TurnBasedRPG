bcfa3a66a9b81e986d20591c940254a0fe52e19c
siweizhu <thinkzhu110929@gmail.com>
2022-06-25 16:01:27 +0800

    Chapter 3
    Control player to move, and add different animations to player.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D theRB;
+    public Rigidbody2D body;
+    public float moveSpeed;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        body.velocity = new Vector2
+        (
+            Input.GetAxisRaw("Horizontal"), 
+            Input.GetAxisRaw("Vertical")
+        ) * moveSpeed;
     }
 }
